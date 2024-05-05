276e8ff9222fc9401a7d36952555637bb2f8f06e
Wu, Sophia Jiaxin <sophiajwu02@gmail.com>
2024-05-05 18:45:48 -0400

    updated contact section

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -5,25 +5,31 @@ const Contact = (): ReactNode => {
     
 
     return (
-        <div className='flex flex-col xl:max-w-5xl md:max-w-xl pt-24' 
+        <div className='flex flex-col justify-center items-center xl:max-w-5xl md:max-w-xl pt-24' 
             id='contact'>
-            <div className='flex justify-between items-center mb-3 w-auto md:mx-5 mx-10' 
-                id='contactHeader'>
-                <h2 className="text-4xl text-primary font-semibold font-lexendExa">
-                    Wanna say hello?
-                </h2>
-                <div className='flex-grow mx-4 border-t-2 border-light-grey invisible md:visible'></div>
+
+            <h2 className="text-4xl text-primary font-semibold font-lexendExa mb-3 md:mx-5 mx-10">
+                Wanna say hello?
+            </h2>
+
+            <div className='text-light-grey text-center font-inter md:mx-32 mx-32 mb-8' 
+                id='contactText'>
+                <p className='mb-4'>
+                I'm currently looking for full-time opportunities. If you think I'd be a good fit for your team, feel free to reach out to me!
+                </p>
+                <p>
+                You can find me on LinkedIn, GitHub, or email me directly at
+                </p>
             </div>
 
-                <div className='grid text-light-grey font-inter gap-y-4 md:mx-10 mx-16 w-1/2 flex items-center justify-center' 
-                    id='contactText'>
-                    <p>
-                    I'm currently looking for full-time opportunities starting in May 2023. If you think I'd be a good fit for your team, feel free to reach out to me!
-                    </p>
-                    <p>
-                    You can find me on LinkedIn, GitHub, or email me directly at
-                    </p>
-                    </div>
+            <a className="relative flex items-center justify-start inline-block px-5 py-3 overflow-hidden font-semibold rounded-full group shadow-btn"
+                    id='email-link'
+                    href={`mailto:${'sophiajwu02@gmail.com'}`}>
+                    <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-700 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-primary opacity-100 group-hover:-translate-x-2"></span>
+                    <span className="relative w-full text-center text-off-white font-lexendExa transition-colors duration-700 ease-in-out group-hover:text-dark-black">Let's Connect!</span>
+                    <span className="absolute inset-0 border-2 border-primary rounded-full"></span>
+            </a>
+            
         </div>
     );
 };
